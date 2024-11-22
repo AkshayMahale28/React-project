93b0c14b60f6a9167720bd81bc395db5d2aa09e3
Akshay Mahale <akshaymahale2830011@gmail.com>
2024-11-22 15:18:00 +0530

    Memory Allocation in Js

---
+++ JAVASCRIPT/stackAndheap.js
@@ -0,0 +1,33 @@
+// // let str="Akshaymahale"
+
+// // let str2=str
+
+// // str2="AmanGupta"
+
+
+// // console.log(str);
+// // console.log(str2);
+
+
+// // non-premitive(Reference Type)
+
+// let userOne={
+//     name:"Akshay",
+//     age:25,
+    
+// }
+
+// let userTwo=userOne
+
+
+// userTwo.name="SSBT COLLAGE"
+
+// console.log(userOne.name);
+// console.log(userTwo.name);
+
+
+
+
+
+
+
