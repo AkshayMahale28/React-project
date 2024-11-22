b477a2b516d19a671bc122d9059c39e4290f1700
Akshay Mahale <akshaymahale2830011@gmail.com>
2024-11-22 11:26:57 +0530

    typeConversion

---
+++ JAVASCRIPT/typrConversion.js
@@ -0,0 +1,39 @@
+// let newNumber="100abc"
+
+
+// let conNumberIs=Number(newNumber)
+
+// console.log(conNumberIs);
+// console.log( typeof conNumberIs);
+
+//Type of Nan is Number
+
+
+// let str="2000"
+
+// let str2=String(str)
+
+// console.log(str2);
+// console.log(typeof str2);
+
+
+//Type of Null is obj
+
+// let newVal=null
+
+// console.log(newVal);
+// console.log(typeof newVal);
+
+
+// let newVal=true
+// let newVal=false
+x
+// let newVal1=Boolean(newVal)
+// let newVal1=Number(newVal)
+
+// console.log(newVal1);
+// console.log(typeof newVal1);
+
+
+
+
