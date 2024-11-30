fdcc309fa6abb8a65c660ef8494f2c8fb6a0082d
Akshay Mahale <akshaymahale2830011@gmail.com>
2024-11-30 11:43:29 +0530

    New Practice File Added

---
+++ practice/practice.js
@@ -0,0 +1,24 @@
+// let a=10
+// let b=20
+// // let temp
+
+// // temp=a
+// // a=b
+// // b=temp
+
+// // console.log(a,b);
+
+
+// a=a+b  //10+20=30
+// b=a-b  //30-20=10
+// a=a-b  //30-10=20
+// console.log(a,b); //20 10
+
+
+
+
+
+
+
+
+
