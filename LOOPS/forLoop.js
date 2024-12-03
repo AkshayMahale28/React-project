f3772355eaaaff00779ea4f9c9b4d0d94a13fd01
Akshay Mahale <akshaymahale2830011@gmail.com>
2024-12-03 15:26:50 +0530

    learn about loops

---
+++ LOOPS/forLoop.js
@@ -0,0 +1,54 @@
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+    
+// }
+
+// for (let index = 1; index < 10; index++) {
+
+//     console.log(`OUTER LOOP AND HIS VALUE:${index}`);
+//     for (let j = 1; j < 10; j++) {
+//         // console.log(`INNER LOOP:${j}`)
+//         console.log(`${index} * ${j} = ${index * j}`)
+        
+//     }
+    
+    
+// }
+
+
+const balance=10
+
+// for (let balance = 1; balance <10; balance++) {
+//     if (balance===5) {
+//         console.log("Detected 5");
+//         break
+        
+        
+//     }
+//     console.log(`Value of one is ${balance}`);
+    
+    
+// }
+// for (let balance = 1; balance <10; balance++) {
+//     if (balance===5) {
+//         console.log("Detected 5");
+//         continue
+        
+        
+//     }
+//     console.log(`Value of one is ${balance}`);
+    
+    
+// }
+
+
+// const myArray=["Akshay","Raj","Aman"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
